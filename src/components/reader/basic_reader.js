bcc188c9d505007c39b92682ead8e6c660e53b1f
yo16 <xxyo1xx@gmail.com>
2022-12-16 16:44:49 +0900

    move process to be instantiated

--- src/components/reader/basic_reader.js
+++ src/components/reader/basic_reader.js
@@ -5,11 +5,12 @@ const YnfDoc = require("../core/ynf_doc");
 class BasicReader{
     // 継承されたクラスでは、ここでファイルパスか、そのものを渡す
     constructor(options){
+        // parseして、ここでdocインスタンスを作成
+        this.doc = new YnfDoc();
     };
 
     get_ynfdoc(optoins){
-        // 継承されたクラスでは、読み込んで作成されたYnfDocを返す
-        return new YnfDoc();
+        return this.doc;
     }
 };
 
