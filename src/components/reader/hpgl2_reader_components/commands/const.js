7085f056e14aa5385d0ed59475127e1b1929fde7
yo16 <xxyo1xx@gmail.com>
2022-12-30 02:31:45 +0900

    add PS

--- src/components/reader/hpgl2_reader_components/commands/const.js
+++ src/components/reader/hpgl2_reader_components/commands/const.js
@@ -2,9 +2,13 @@
 */
 
 module.exports = {
+    // BP
     HPGL2_OPT_PICTURE_NAME: 'picture_name',
     HPGL2_OPT_NUMBER_OF_COPIES: 'number_of_copies',
     HPGL2_OPT_FILE_DISPOSITION_CODE: 'file_disposition_code',
     HPGL2_OPT_RENDER_LAST_PLOT: 'render_last_plot',
     HPGL2_OPT_AUTOROTATION: 'autorotation',
+    // PS
+    HPGL2_OPT_PLOTSIZE_WIDTH: 'plotsize_width',
+    HPGL2_OPT_PLOTSIZE_LENGTH: 'plotsize_length',
 }
--- src/components/reader/hpgl2_reader_components/commands/one_command_PS.js
+++ src/components/reader/hpgl2_reader_components/commands/one_command_PS.js
@@ -3,12 +3,28 @@
 */
 
 const OneCommandBase = require('./one_command_base')
+const const_value = require('./const')
 
 class OneCommandPS extends OneCommandBase{
     constructor(options){
         super(options)
     }
     
+    // 設定
+    parse_options(options) {
+        let ret = {}
+        if ( this.command_param.length===0 ) { return ret }
+
+        let ary = this.command_param.split(',')
+        if ( ary.length > 0 ) {
+            ret[const_value.HPGL2_OPT_PLOTSIZE_WIDTH] = ary[0]
+        }
+        if ( ary.length > 1 ) {
+            ret[const_value.HPGL2_OPT_PLOTSIZE_LENGTH] = ary[1]
+        }
+
+        return ret
+    }
 }
 
 module.exports = OneCommandPS
